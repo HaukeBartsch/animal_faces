81ea52c6668387fb0a55a3b684624f704fa502ae
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-03-03 15:24:35 +0100

    fix filename extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,8 @@ eval $cmd
 # The above call should create a jpg image for each empty jpg file. We will look for all that do not have a png yet.
 for u in `ls /data/*.jpg`; do
     echo "Try to create a circular cutout for ${u}."
-    magick "${u}" -gravity center \( \( "${u}" -fill black -draw "rectangle 0,0 %[fx:w],%[fx:h]" \) xc:black -fill white -draw "circle %[fx:w/2] %[fx:h/2] %[fx:w/2] 0" -alpha copy -composite \) -compose copyopacity -composite "${u##*.}.png"
+    fn=$(basenameee -- "${u}")
+    magick "${u}" -gravity center \( \( "${u}" -fill black -draw "rectangle 0,0 %[fx:w],%[fx:h]" \) xc:black -fill white -draw "circle %[fx:w/2] %[fx:h/2] %[fx:w/2] 0" -alpha copy -composite \) -compose copyopacity -composite "/data/${fn%.*}.png"
 done
 
 echo "$(date): processing done"
